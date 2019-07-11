7ef587a19baef4afce478d6463478ded329f8895
Kevin Jablonka <kevin.jablonka@epfl.ch>
2019-07-11 20:09:17 +0200

    reformatted.

--- papersbot.py
+++ papersbot.py
@@ -26,7 +26,7 @@ import tweepy
 # This is the regular expression that selects the papers of interest
 # Exclude bio stuff, ML also used in surface science, so we also include this
 regex = re.compile(
-        r"""^(?!.*(cell|clinic|bio|histological|medic|bacterial|organ|metabolic|gene|injury|living|ontolog|tumor|cancer|brain)).*(
+    r"""^(?!.*(cell|clinic|bio|histological|medic|bacterial|organ|metabolic|gene|injury|living|ontolog|tumor|cancer|brain)).*(
     machine.learning 
     | deep.learning
     | neural.network
